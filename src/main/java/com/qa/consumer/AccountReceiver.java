aab28034ae7e9d73637ce2a54af7bc26a35f33d5
andrewsa123 <andrewknott_123@hotmail.co.uk>
2018-11-30 13:27:23 +0000

    CORS

--- src/main/java/com/qa/consumer/AccountReceiver.java
+++ src/main/java/com/qa/consumer/AccountReceiver.java
@@ -3,11 +3,13 @@ package com.qa.consumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.qa.ATS.Account;
 import com.qa.service.AccountService;
 
 @Component
+@CrossOrigin
 public class AccountReceiver {
 	
 	@Autowired
